d7ec215508777a61d2a469fa5a24e6f31454e433
TechnorobAssist <TechnorobAssist@DESKTOP-J6N3EHQ>
2019-11-11 09:48:30 +0300

    update

--- DinamikFormYonetimi/DinamikFormYonetimi/Controllers/FormController.cs
+++ DinamikFormYonetimi/DinamikFormYonetimi/Controllers/FormController.cs
@@ -16,7 +16,7 @@ namespace DinamikFormYonetimi.Controllers
         FormDBEntities db = new FormDBEntities();
 
     
-
+        //yorumm
         public ActionResult Index()
         {
             //List<Form> frmList = db.Forms.ToList();
